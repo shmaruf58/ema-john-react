11eca07502a988dd8cc46d2892f4de57ebd2f649
shmaruf58 <maruf414100masum@gmail.com>
2023-04-05 14:46:25 +0600

    try commit

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -17,7 +17,7 @@ const Shop = () => {
     useEffect( () =>{
         const storedCart = getShoppingCart();
         const savedCart = [];
-        // step 1: get id of the addedProduct
+        // step 1: get id of the added the Product
         for(const id in storedCart){
             // step 2: get product from products state by using id
             const addedProduct = products.find(product => product.id === id)
